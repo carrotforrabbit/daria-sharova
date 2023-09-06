738496077990e52e5661da3c4708f66a7605373c
carrotforrabbit <parasyuta.anna@gmail.com>
2023-09-06 22:33:12 +0100

    add file preview

--- assets/scripts/form.js
+++ assets/scripts/form.js
@@ -8,7 +8,24 @@ document.addEventListener("DOMContentLoaded", function () {
     e.preventDefault();
 
     let error = formValidate(form);
+
+    let formData = new FormData(form);
+    formData.append("image", formReferenceImg.files[0]);
+
     if (error === 0) {
+      form.classList.add("._sending");
+      let response = await fetch("sendemail.php", {
+        method: "POST",
+        body: formData,
+      });
+      if (response.ok) {
+        let result = await response.json();
+        alert(result.message);
+        formReferenceImg.innerHTML = "";
+        form.reset();
+      } else {
+        alert("Error");
+      }
     } else {
       alert("Please fill in the required fields");
     }
@@ -58,11 +75,41 @@ document.addEventListener("DOMContentLoaded", function () {
     input.classList.remove("_error");
   }
 
+  //email validation
   function emailTest(input) {
     return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
   }
 
+  //date validation
   function dateTest(input) {
     return !/^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2,4}$/.test(input.value);
+  }
+
+  //img preview
+  const formReferenceImg = document.getElementById("formReferenceImg");
+  const referenceImgPreview = document.getElementById("referenceImgPreview");
+
+  formReferenceImg.addEventListener("change", () => {
+    uploadFile(formReferenceImg.files[0]);
+  });
+
+  function uploadFile(file) {
+    if (!["image/jpeg", "image/png", "image/gif"].includes(file.type)) {
+      alert("Please upload .img, .png, .gif files");
+      formReferenceImg.value = "";
+      return;
+    }
+    if (file.size > 2 * 1024 * 1024) {
+      alert("The file should not exceed 2MB");
+      return;
+    }
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      referenceImgPreview.innerHTML = `<img src="${e.target.result}" alt="Photo">`;
+    };
+    reader.onerror = function (e) {
+      alert("Error");
+    };
+    reader.readAsDataURL(file);
   }
 });
