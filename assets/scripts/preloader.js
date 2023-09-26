69b06d086c85b6aa43c94e54fd4cc0ac6218c8a7
carrotforrabbit <parasyuta.anna@gmail.com>
2023-09-26 01:34:13 +0100

    fixes

--- assets/scripts/preloader.js
+++ assets/scripts/preloader.js
@@ -1,11 +1,3 @@
-// window.onload = function () {
-//   let preloader = document.getElementById("preloader");
-//   preloader.classList.add("hide-preloader");
-//   setInterval(function () {
-//     preloader.classList.add("preloader-hidden");
-//   }, 990);
-// };
-
 window.addEventListener("load", () => {
   const preloader = document.querySelector(".preloader");
   preloader.classList.add("preloader_hidden");
